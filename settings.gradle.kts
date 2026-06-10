0f6dc11aefb2d6d1f85546cbb88c17ff935c771f
Elena Blascu <elena21.blascu@gmail.com>
2026-06-10 15:05:28 +0300

    changed the button, completed communication layer

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("com.android.settings") version "9.2.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Go2Controller"
+include(":android_app:app")
+project(":android_app:app").name = "app"
